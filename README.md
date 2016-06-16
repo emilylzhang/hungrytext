afd4786b818a512e432c010ccd40837833c1947a
Emily Liu Zhang <e.zhang@berkeley.edu>
2016-06-16 13:05:47 -0700

    finally got everything to work! PLUS now also gives you the directions to the nearest In-N-Out!! :>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # hungrytext
-Replies to a text to the number [(669) 600-5660] containing your current location with a text containing the location of the nearest In-N-Out. Built using heroku and twilio.
+Replies to a text to the number [(669) 600-5660] containing your current location with a text containing the location of the nearest In-N-Out, as well as directions to this location. Built using heroku and twilio.
 
 (Only works for phone numbers that have been registered to twilio.)
--- run.py
+++ run.py
@@ -1,18 +1,38 @@
 from flask import Flask, request, redirect, send_from_directory
 import twilio.twiml
 import os
-# import googlemaps
+
+# import googlemaps and geocoder
 import geocoder
+import googlemaps
+from datetime import datetime
 
-# googlemaps key
-# it was here before i took it out
+gmaps = googlemaps.Client(key=os.getenv('GOOGLE_SERVER_API_KEY'))
 
-# Download the twilio-python library from http://twilio.com/docs/libraries
-# from twilio.rest import TwilioRestClient
+# download the twilio-python library from http://twilio.com/docs/libraries
+# and create a client object to use the twilio REST api
+from twilio.rest import TwilioRestClient
+
+twilioclient = TwilioRestClient(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN')) 
 
 # create a client object to use Yelp API
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
+
+auth = Oauth1Authenticator(
+    consumer_key=os.getenv('YELP_CONSUMER_KEY'),
+    consumer_secret=os.getenv('YELP_CONSUMER_SECRET'),
+    token=os.getenv('YELP_TOKEN_KEY'),
+    token_secret=os.getenv('YELP_TOKEN_SECRET')
+)
+yelpclient = Client(auth)
+params = {
+    'term': 'In-N-Out Burger',
+    'lang': 'en',
+    'limit': 1,     # limit 1 business result
+    'sort': 0,      # sort by best match
+    'category_filter': 'burgers'
+}
 
 # create a Flask app
 app = Flask(__name__)
@@ -20,16 +40,15 @@ app = Flask(__name__)
 # begin here
 @app.route("/", methods=['GET'])
 def kek():
-    return "READ THE FUCKING DOCUMENTATION U FUCKING SLUT AHHHHhhHH"
+    return "READ THE FUCKING DOCUMENTATION AHHHHhhHH"
 
 @app.route("/", methods=['POST'])
 def hello():
-    print(os.getenv('YELP_CONSUMER_KEY'))
     lat = 0
     lng = 0
     burgeraddr = 0
 
-    """Respond to incoming calls with a simple text message."""
+    # respond to incoming calls with a simple text message
     resp = twilio.twiml.Response()
 
     # get the number that the request is from
@@ -48,53 +67,94 @@ def hello():
     lat = geocode_result.latlng[0]
     lng = geocode_result.latlng[1]
     latlng = str(geocode_result.latlng).strip('[]')
-    print(0)
 
+    # if POST request has been made with proper lat and lng
     if lat != 0 and lng != 0:
-        print(1)
-        auth = Oauth1Authenticator(
-            consumer_key=os.getenv('YELP_CONSUMER_KEY'),
-            consumer_secret=os.getenv('YELP_CONSUMER_SECRET'),
-            token=os.getenv('YELP_TOKEN_KEY'),
-            token_secret=os.getenv('YELP_TOKEN_SECRET')
-        )
-        print(2)
-        client = Client(auth)
-        print(3)
-        params = {
-            'term': 'In-N-Out Burger',
-            'lang': 'en',
-            'limit': 1,     # limit 1 business result
-            'sort': 0,      # sort by best match
-            'category_filter': 'burgers'
-        }
-        print(4)
-        burgerplace = client.search_by_coordinates(lat, lng, **params)
-        print(5)
-        burgeraddr = burgerplace.businesses[0].location.display_address
-        address = ""
-        while len(burgeraddr) != 0:
-            address = burgeraddr.pop() + ", " + address
-        address = address[0:len(address)-2]
-        print(address)
+        burgerplace = yelpclient.search_by_coordinates(lat, lng, **params)
+        if len(burgerplace.businesses) != 0:
+            burgeraddr = burgerplace.businesses[0].location.display_address
+            address = ""
+            while len(burgeraddr) != 0:
+                address = burgeraddr.pop() + ", " + address
+            address = address[0:len(address)-2]
+            print(address)
+        else:
+            address = "YOU DID NOT GIVE A VALID ADDRESS"
 
-    resp.message("yo, this is the location of the nearest In-N-Out: " +address)
+    # create response message
+    # text(from_number, "yo, this is the location of the nearest In-N-Out: " +address)
+    resp.message("...............................\n\n\nyo, this is the location of the nearest In-N-Out: " +address)
 
-    # resp.message("yo, this is what you wrote: " +body +"\nAnd this is the latitude and longitude of the location you texted: (" +latlng +")\nAnd this is the address of the nearest In-N-Out: " +burgeraddr)
-    # below is when i was using the google geocoding api -
-        # geocode_result = gmaps.geocode(body)
-        # print("geocode_result: " + geocode_result)
-        # print("formatted address: " + geocode_result.formatted_address)
+    # text directions to the nearest In-n-Out
+    directions(from_number, address, body)
 
     print("response: " + str(resp))
     return str(resp)
+
+# finds directions to the nearest In-N-Out
+def directions(number, to_address, from_address):
+    now = datetime.now()
+    directions_result = gmaps.directions(origin=from_address,
+                                     destination=to_address,
+                                     mode="driving",
+                                     departure_time=now)
+    leg = directions_result[0]['legs']
+    steps = leg[0]['steps']
+    directiontext = ""
+    for i in range(0, len(steps)):
+        # test the part below
+        step = steps[i]['html_instructions']
+        print(step)
+        step = remove_html_markup(step)
+        directiontext = directiontext +str(i+1) +") " +step +"\n\n"
+    text(number, directiontext)
+
+# sends a text to the number given, with textbody as the content
+def text(number, textbody):
+    twilioclient.messages.create(
+        to=number, 
+        from_="+16696005660", 
+        body="...............................\n\n\n"+textbody, 
+        media_url="http://media2.fdncms.com/sacurrent/imager/new-headline/u/slideshow/2346373/in-n-out-burger.jpg", 
+    )
+
+# remove HTML tags from the directions
+# taken from http://stackoverflow.com/a/14464381
+def remove_html_markup(s):
+    tag = False
+    quote = False
+    out = ""
+    div1 = False
+    div2 = False
+
+    for c in s:
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif (c == '"' or c == "'") and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+        elif tag and c == 'd':
+            div1 = True
+        elif tag and div1 and c == 'i':
+            div2 = True
+        elif tag and div1 and div2 and c == 'v':
+            out = out + "\n"
+            div1 = False
+            div2 = False
+        else:
+            div1 = False 
+            div2 = False
+
+    return out
 
 # handle favicon
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/png/vnd.microsoft.icon')
-
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
